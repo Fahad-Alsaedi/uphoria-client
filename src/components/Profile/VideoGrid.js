026e89e9377023904162aa5e662b7036df8d62ec
kinawy <sameh.yasser.kinawy@gmail.com>
2020-09-07 10:40:02 -0400

    Testing video query

--- src/components/Profile/VideoGrid.js
+++ src/components/Profile/VideoGrid.js
@@ -1,54 +1,60 @@
 import React from "react"
-import {makeStyles} from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import Paper from "@material-ui/core/Paper"
 import Grid from "@material-ui/core/Grid"
+import gql from "graphql-tag"
+import { graphql } from "react-apollo"
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   video: {
     padding: theme.spacing(1),
     textAlign: "center",
     width: "100%",
     position: "relative",
-  }
+  },
 }))
 
 const VideoGrid = (props) => {
   const classes = useStyles()
+  let renderVideos = () => {
+    console.log(props.data)
+    let videos = props.data.videos.map((video, i) => {
+      return (
+        <Grid item xs={4}>
+          <img key={i} src={video.videoUrl} className={classes.video} />
+        </Grid>
+      )
+    })
+    return videos
+  }
+  if (props.data.loading) {
+    return <div>Loading...</div>
+  }
 
   function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <img src="https://via.placeholder.com/200x350" className={classes.video}/>
-        </Grid>
-        <Grid item xs={4}>
-          <img src="https://via.placeholder.com/200x350" className={classes.video}/>
-        </Grid>
-        <Grid item xs={4}>
-          <img src="https://via.placeholder.com/200x350" className={classes.video}/>
-        </Grid>
-      </React.Fragment>
-    )
+    return <React.Fragment>{renderVideos()}</React.Fragment>
   }
 
   return (
     <div className={classes.root}>
       <Grid container spacing={1}>
         <Grid container item s={12} spacing={3}>
-          <FormRow/>
-        </Grid>
-        <Grid container item s={12} spacing={3}>
-          <FormRow/>
-        </Grid>
-        <Grid container item s={12} spacing={3}>
-          <FormRow/>
+          <FormRow />
         </Grid>
       </Grid>
     </div>
   )
 }
 
-export default VideoGrid
+const query = gql`
+  {
+    videos {
+      videoUrl
+    }
+  }
+`
+
+export default graphql(query)(VideoGrid)
